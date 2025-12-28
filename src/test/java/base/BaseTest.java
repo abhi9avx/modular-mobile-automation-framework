366e63d2f88c37aa8b52a876b50efff1b1990517
ABHINAV KUMAR <56204649+abhi9avx@users.noreply.github.com>
2025-12-28 12:45:34 +0530

    feat: Capture screenshots on test success and failure using Allure attachments in `tearDown`.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,6 +1,10 @@
 package base;
 
 import core.DriverFactory;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -14,9 +18,22 @@ public class BaseTest {
   }
 
   // This method runs AFTER every test method
-  @AfterMethod
-  public void tearDown() {
-    // Quit the Appium driver and close the app
+  @AfterMethod(alwaysRun = true)
+  public void tearDown(ITestResult result) {
+
+    if (ITestResult.SUCCESS == result.getStatus()) {
+      captureScreenshot("SUCCESS Screenshot");
+    }
+
+    if (ITestResult.FAILURE == result.getStatus()) {
+      captureScreenshot("FAILURE Screenshot");
+    }
+
     DriverFactory.quitDriver();
+  }
+
+  @Attachment(value = "{screenshotName}", type = "image/png")
+  public byte[] captureScreenshot(String screenshotName) {
+    return ((TakesScreenshot) DriverFactory.getDriver()).getScreenshotAs(OutputType.BYTES);
   }
 }
